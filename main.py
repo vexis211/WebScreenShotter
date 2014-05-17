bb8fe6616de076b38a90423732121fa38b9f1fdf
Jan Skalicky <hskalicky@gmail.com>
2014-05-17 17:20:52 +0200

    deleted index.html

--- main.py
+++ main.py
@@ -9,20 +9,19 @@ from webscreenshots import get_last_screenshots, find_screenshots, get_screensho
 
 class MainHandler(BaseHandler):
     def get(self):
-        self.render_response('index.html')
-        # searching_uri = self.request.get('searchingUri')
-        # if searching_uri:
-        #     screenshots = find_screenshots(searching_uri)
-        #     template_values = {
-        #         'screenshots': screenshots
-        #     }
-        #     self.render_response('parts/screenshot_table.html', template_values)
-        # else:
-        #     screenshots = get_last_screenshots()
-        #     template_values = {
-        #         'screenshots': screenshots
-        #     }
-        #     self.render_response('pages/home.html', template_values)
+        searching_uri = self.request.get('searchingUri')
+        if searching_uri:
+            screenshots = find_screenshots(searching_uri)
+            template_values = {
+                'screenshots': screenshots
+            }
+            self.render_response('parts/screenshot_table.html', template_values)
+        else:
+            screenshots = get_last_screenshots()
+            template_values = {
+                'screenshots': screenshots
+            }
+            self.render_response('pages/home.html', template_values)
 
 
 class ImagesHandler(BaseHandler):
