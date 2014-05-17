50204bc096dbdf0d929e3af36fc0e87b8fa67484
Jan Skalicky <hskalicky@gmail.com>
2014-05-17 17:13:35 +0200

    error - try to fix

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ from webscreenshots import get_last_screenshots, find_screenshots, get_screensho
 
 class MainHandler(BaseHandler):
     def get(self):
-        searching_uri = self.request.get('searchingUri')
-        if searching_uri:
-            screenshots = find_screenshots(searching_uri)
-            template_values = {
-                'screenshots': screenshots
-            }
-            self.render_response('parts/screenshot_table.html', template_values)
-        else:
-            screenshots = get_last_screenshots()
-            template_values = {
-                'screenshots': screenshots
-            }
-            self.render_response('pages/home.html', template_values)
+        self.render_response('index.html')
+        # searching_uri = self.request.get('searchingUri')
+        # if searching_uri:
+        #     screenshots = find_screenshots(searching_uri)
+        #     template_values = {
+        #         'screenshots': screenshots
+        #     }
+        #     self.render_response('parts/screenshot_table.html', template_values)
+        # else:
+        #     screenshots = get_last_screenshots()
+        #     template_values = {
+        #         'screenshots': screenshots
+        #     }
+        #     self.render_response('pages/home.html', template_values)
 
 
 class ImagesHandler(BaseHandler):
